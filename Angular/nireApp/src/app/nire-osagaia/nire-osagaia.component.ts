708f10de8321288bfcd6264b1e5a78215363de2d
Markel Rodriguez <96288766+MarkelRodriguez@users.noreply.github.com>
2023-11-24 12:56:05 +0100

    helloWorld eta helloTabs eginda

--- Angular/nireApp/src/app/nire-osagaia/nire-osagaia.component.ts
+++ Angular/nireApp/src/app/nire-osagaia/nire-osagaia.component.ts
@@ -17,13 +17,26 @@ export class NireOsagaiaComponent {
   }
   
   langileak = [
-    { izena: "Pepe", abizena: "Perez", generoa: "gizona", soldata: 1000, likes: 0, lanean: true},
-    { izena: "Sara", abizena: "Ruiz", generoa: "emakumea", soldata: 1000, likes: 0, lanean: true},
-    { izena: "Paul", abizena: "Elorza", generoa: "gizona", soldata: 1500, likes: 0, lanean: false},
-    { izena: "Raul", abizena: "Txakartegi", generoa: "gizona", soldata: 2000, likes: 0, lanean: false}
+    { izena: "Pepe", abizena: "Perez", generoa: "gizona", soldata: 1000, likes: 0, lanean: true, jaiotza: new Date(24/11/2023)},
+    { izena: "Sara", abizena: "Ruiz", generoa: "emakumea", soldata: 1000, likes: 0, lanean: true, jaiotza: new Date(24/11/2023)},
+    { izena: "Paul", abizena: "Elorza", generoa: "gizona", soldata: 1500, likes: 0, lanean: false, jaiotza: new Date(24/11/2023)},
+    { izena: "Raul", abizena: "Txakartegi", generoa: "gizona", soldata: 2000, likes: 0, lanean: false, jaiotza: new Date(24/11/2023)}
     ];
 
     gehituLikes(langilea: any){ 
       langilea.likes++; 
     }
+    myText = "Nire izena: Markel da";
+
+    rowStart = 0;
+    rowLimit = 2;
+    kenduRow() {
+    this.rowStart -= 2;
+    this.rowLimit -= 2;
+    }
+    gehituRow() {
+    this.rowStart += 2;
+    this.rowLimit += 2;
+}
+
 }
