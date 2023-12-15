616ef84aa50212ec79d9450cfbd5dddc0aab901d
Markel Rodriguez <96288766+MarkelRodriguez@users.noreply.github.com>
2023-12-15 14:23:24 +0100

    ionic api

--- Ionic/IonicStrava/src/app/tab1/tab1.page.ts
+++ Ionic/IonicStrava/src/app/tab1/tab1.page.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { KlubaService} from '../services/kluba.service';
+import { ApiService} from '../services/api.service';
+import { Kluba } from '../classes/kluba';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+  klubak: Kluba[] = [];
+  showLoader=true;
+  //laravel rest apia
+  //constructor(private klubaService: KlubaService) {}
+  
+  //sqlite
+  constructor(private apiService: ApiService) {}
 
-  constructor() {}
+  /*
+  getKlubak(): void{
+    this.klubaService.getKlubak()
+      .subscribe(data => {this.klubak = data; },
+          error => console.log('Error::' + error)); }
 
-  kluba = {
+  */
+  getKlubak(): void{
+    this.apiService.dbState().subscribe((res) => {
+      if(res){
+        this.apiService.fetchKlubak().subscribe(
+          data => {this.klubak = data;
+          this.showLoader=false;}
+        )
+      }
+     });
+  }
+  
+  ngOnInit(): void {
+    this.getKlubak();
+  }
+
+  /*kluba = {
     id: 783189,
     name: 'uni eibar',
     cover_photo_small: 'assets/img/generikoa.jpg',
@@ -18,6 +47,6 @@ export class Tab1Page {
     member_count: 3,
     description: 'Lanbide Heziketako Ikastetxea',
     club_type: 'company',
-  };
+  };*/
 
 }
